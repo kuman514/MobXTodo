e2f75e05e0ae59c86c941080ac12bcdf82af81a9
kumankoishi.py <7iggif@naver.com>
2022-06-28 20:12:47 +0900

    feat(todo-item): add todo content

---
+++ src/components/atoms/TodoContent/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { HEIGHT, FONT_SIZE } from 'properties/height';
+
+const TodoContent = styled.span`
+  margin: 0;
+  box-sizing: border-box;
+  height: ${HEIGHT};
+  font-size: ${FONT_SIZE};
+`;
+
+export default TodoContent;
