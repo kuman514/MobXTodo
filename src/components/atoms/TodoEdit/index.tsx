a04e30eff5435ba64e8232b2825ec29bb006f4bd
kumankoishi.py <7iggif@naver.com>
2022-06-29 21:17:21 +0900

    feat(todo-item): add todo edit input

---
+++ src/components/atoms/TodoEdit/index.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+import { HEIGHT, FONT_SIZE } from 'properties/height';
+
+const TodoEdit = styled.input`
+  margin: 0;
+  box-sizing: border-box;
+  height: ${HEIGHT};
+  font-size: ${FONT_SIZE};
+`;
+
+export default TodoEdit;
