dbaccd91a63b8922a5fdc507b3d232c208afd54c
kumankoishi.py <7iggif@naver.com>
2022-07-06 20:14:49 +0900

    feat(todo-item): pseudo model for todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import TodoCreate from 'components/molecules/TodoCreate';
+import TodoList from 'components/organisms/TodoList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TodoList
+          todos={[
+            { todoId: 0, todoContent: 'Lorem ipsum' },
+            { todoId: 1, todoContent: 'Dolor sit amet' },
+            { todoId: 2, todoContent: 'Consectetur adipiscing elit' },
+            { todoId: 3, todoContent: 'Quisque at mi vel' },
+            { todoId: 4, todoContent: 'Nisl pellentesque placerat' },
+            { todoId: 5, todoContent: 'Cras eu sagittis erat' },
+            { todoId: 6, todoContent: 'Duis et elit' },
+            { todoId: 7, todoContent: 'Rutrum mollis' },
+            { todoId: 8, todoContent: 'Lorem at rhoncus lacus' },
+            { todoId: 9, todoContent: 'Maecenas ornare gravida imperdiet' },
+          ]}
+        />
+        <TodoCreate />
       </header>
     </div>
   );
