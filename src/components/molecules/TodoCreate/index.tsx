d5503fa53900b599564b996dc010e644f1c85a54
kumankoishi.py <7iggif@naver.com>
2022-07-09 00:13:42 +0900

    feat(todo-item): add create feature

--- src/components/molecules/TodoCreate/index.tsx
+++ src/components/molecules/TodoCreate/index.tsx
@@ -1,15 +1,42 @@
 import { FC } from 'react';
+import { observer, useLocalObservable } from 'mobx-react';
 
 import TodoInput from 'components/atoms/TodoInput';
 import TodoButton from 'components/atoms/TodoButton';
 import { Create } from 'properties/color';
+import todoStore from 'store/todoStore';
+
+interface TodoCreateState {
+  writing: string;
+  onWrite: (newWrite: string) => void;
+}
 
 const TodoCreate: FC<{}> = () => {
+  const state = useLocalObservable<TodoCreateState>(() => ({
+    writing: '',
+    onWrite(newWrite) {
+      this.writing = newWrite;
+    },
+  }));
+
+  const handleWrite = (e: React.FormEvent<HTMLInputElement>) => {
+    state.onWrite(e.currentTarget.value);
+  };
+
+  const onAddTodo = () => {
+    todoStore.createTodo(state.writing);
+    state.onWrite('');
+  };
+
   return (
     <div>
-      <TodoInput />
+      <TodoInput
+        value={state.writing}
+        onInput={handleWrite}
+      />
       <TodoButton
         color={Create.MAIN_COLOR}
+        onClick={onAddTodo}
       >
         Create
       </TodoButton>
@@ -17,4 +44,4 @@ const TodoCreate: FC<{}> = () => {
   );
 };
 
-export default TodoCreate;
+export default observer(TodoCreate);
